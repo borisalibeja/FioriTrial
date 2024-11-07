b89afe20cc2414c754c3f11df69d65fd626dca0a
Boris Alibeja <borissalibeja@gmail.com>
2024-11-07 20:18:03 +0000

    upload view2.xml/view2.controller

--- trial4/webapp/controller/View1.controller.js
+++ trial4/webapp/controller/View1.controller.js
@@ -1,43 +1,43 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel",
-    // "../model/dataSet.js"
-],
-    // function (Controller, JSONModel, dataSet) {
-     function (Controller, JSONModel) {
-        "use strict";
+// sap.ui.define([
+//     "sap/ui/core/mvc/Controller",
+//     "sap/ui/model/json/JSONModel",
+//     // "../model/dataSet.js"
+// ],
+//     // function (Controller, JSONModel, dataSet) {
+//      function (Controller, JSONModel) {
+//         "use strict";
 
-        return Controller.extend("trial4.controller.View1", {
-            onInit: function () {
-                const url = "https://run.mocky.io/v3/60441514-2cd0-47c7-9510-4c9362db2b8e"
-                const oModel = new JSONModel()
-                oModel.loadData(url)
-                this.getView().setModel(oModel, "Products")
-                this.callToDB()
-            },
-            callToDB: async function () {
-                let that = this;
-                return new Promise(function (resolve, reject) {
-                    let appId = that.getOwnerComponent().getManifestEntry("/sap.app/id");
-                    let appPath = appId.replaceAll(".", "/");
-                    let appModulePath = jQuery.sap.getModulePath(appPath);
-                    $.ajax({
-                        url: appModulePath + "/odata/sap/opu/odata/sap/ZBA_TEST_PROJECT_SRV/zba_testSet",
-                        type: "GET",
-                        dataType: "json",
-                        success: function (data) {
-                            resolve(that.getView().setModel(new JSONModel(data.d.results), "listModel"));
-                        },
-                        error: function (err, textStatus) {
-                            reject(MessageBox.error(textStatus));
-                        }
-                    });
-                });
-                this.getView().getModel("listModel")?.refresh()
+//         return Controller.extend("trial4.controller.View1", {
+//             onInit: function () {
+//                 const url = "https://run.mocky.io/v3/60441514-2cd0-47c7-9510-4c9362db2b8e"
+//                 const oModel = new JSONModel()
+//                 oModel.loadData(url)
+//                 this.getView().setModel(oModel, "Products")
+//                 this.callToDB()
+//             },
+//             callToDB: async function () {
+//                 let that = this;
+//                 return new Promise(function (resolve, reject) {
+//                     let appId = that.getOwnerComponent().getManifestEntry("/sap.app/id");
+//                     let appPath = appId.replaceAll(".", "/");
+//                     let appModulePath = jQuery.sap.getModulePath(appPath);
+//                     $.ajax({
+//                         url: appModulePath + "/odata/sap/opu/odata/sap/ZBA_TEST_PROJECT_SRV/zba_testSet",
+//                         type: "GET",
+//                         dataType: "json",
+//                         success: function (data) {
+//                             resolve(that.getView().setModel(new JSONModel(data.d.results), "listModel"));
+//                         },
+//                         error: function (err, textStatus) {
+//                             reject(MessageBox.error(textStatus));
+//                         }
+//                     });
+//                 });
+//                 this.getView().getModel("listModel")?.refresh()
                
-            },
+//             },
 
-        });
+//         });
 
-    });
+//     });
 
---
+++ trial4/webapp/controller/View2.controller.js
@@ -0,0 +1,23 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    // "../model/dataSet.js"
+],
+    // function (Controller, JSONModel, dataSet) {
+     function (Controller, JSONModel) {
+        "use strict";
+
+        return Controller.extend("trial4.controller.View2", {
+            onInit: function () {
+                const url = "https://run.mocky.io/v3/60441514-2cd0-47c7-9510-4c9362db2b8e"
+                const oModel = new JSONModel()
+                oModel.loadData(url)
+                this.getView().setModel(oModel, "Products")
+                
+            },
+
+        });
+    }
+)
+    
+
