b8bee8d43662c9b9621ebf60fbb597246059d753
Boris Alibeja <borissalibeja@gmail.com>
2024-11-07 16:12:39 +0000

    odata configuration

--- trial4/webapp/controller/View1.controller.js
+++ trial4/webapp/controller/View1.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    // "../model/dataSet.js"
 ],
-    function (Controller, JSONModel) {
+    // function (Controller, JSONModel, dataSet) {
+     function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("trial4.controller.View1", {
@@ -11,6 +13,31 @@ sap.ui.define([
                 const oModel = new JSONModel()
                 oModel.loadData(url)
                 this.getView().setModel(oModel, "Products")
-            }
+                this.callToDB()
+            },
+            callToDB: async function () {
+                let that = this;
+                return new Promise(function (resolve, reject) {
+                    let appId = that.getOwnerComponent().getManifestEntry("/sap.app/id");
+                    let appPath = appId.replaceAll(".", "/");
+                    let appModulePath = jQuery.sap.getModulePath(appPath);
+                    $.ajax({
+                        url: appModulePath + "/odata/sap/opu/odata/sap/ZBA_TEST_PROJECT_SRV/zba_testSet",
+                        type: "GET",
+                        dataType: "json",
+                        success: function (data) {
+                            resolve(that.getView().setModel(new JSONModel(data.d.results), "listModel"));
+                        },
+                        error: function (err, textStatus) {
+                            reject(MessageBox.error(textStatus));
+                        }
+                    });
+                });
+                this.getView().getModel("listModel")?.refresh()
+               
+            },
+
         });
+
     });
+
